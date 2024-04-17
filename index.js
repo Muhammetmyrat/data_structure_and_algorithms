fba9f0fc9d447e24876c9dae7f6924a7c4e1122e
Muhammetmyrat <bayramovmuhammetmyrat97@gmail.com>
2024-04-17 18:46:54 +0500

    Big O notation

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 //Fibonacci
 
+// O(2^n)
 const fibNative = (n) => {
   if (n <= 1) return n
   return fibNative(n - 1) + fibNative(n - 2)
 }
 
+// O(n)
 const fibEffective = (n) => {
+  // O(n)
   const arr = []
   arr[0] = 0
   arr[1] = 1
 
+  // n = 2 => 1 iteration
+  // n = 3 => 2 iteration
+  // n = 4 => 3 iteration
+  // n = 5 => 4 iteration
+
+  // O(n + n) => O(2n) => O(n)
   for (i = 2; i <= n; i++) {
     arr[i] = arr[i - 1] + arr[i - 2]
   }
@@ -21,6 +30,7 @@ console.log(fibEffective(100))
 
 //Memoization
 
+// O(2^n)
 const fibNativeMemoization = (n, mem) => {
   if (mem[n] !== -1) return mem[n]
   if (n <= 1) return n
